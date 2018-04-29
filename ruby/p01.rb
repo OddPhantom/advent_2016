0d0c0de208ee8b8d3e9213652dd0ec6b91237ea5
Todd Jefferson <todd.jefferson@shopify.com>
2018-04-29 12:17:57 -0400

    make ruby p01 more OO by adding a person

--- ruby/p01.rb
+++ ruby/p01.rb
@@ -2,12 +2,35 @@
 require_relative 'base'
 require 'pry-byebug'
 
-Location = Struct.new(:x, :y) do
-  def away
-    x.abs + y.abs
+class Person
+  attr_reader :orientation, :position
+  def initialize()
+    @orientation = Direction.new(0, 1)
+    @position = Location.new(0, 0)
   end
-  def to_s
-    "(#{x}, #{y})"
+
+  def follow_instruction(instruction)
+    turn(instruction.turn_direction)
+    move(instruction.count)
+  end
+
+  def move(count)
+    @position = Location.new(position.x + orientation.x * count,
+                             position.y + orientation.y * count)
+  end
+
+  def turn(direction)
+    send(direction)
+  end
+
+  private
+
+  def l
+    @orientation = Direction.new(-1 * orientation.y,  orientation.x)
+  end
+
+  def r
+    @orientation = Direction.new(orientation.y, -1 * orientation.x)
   end
 end
 
@@ -17,6 +40,16 @@ Direction= Struct.new(:x, :y) do
   end
 end
 
+Location = Struct.new(:x, :y) do
+  def away
+    x.abs + y.abs
+  end
+  def to_s
+    "(#{x}, #{y})"
+  end
+end
+
+
 class Instruction
   attr_reader :turn_direction, :count
 
@@ -25,41 +58,20 @@ class Instruction
     @turn_direction = instruction[0].downcase
     @count = instruction[1..-1].to_i
   end
-
-  def turn(o)
-    send(turn_direction, o)
-  end
-
-  def move(o, location)
-    Location.new(location.x + o.x * count,
-                 location.y + o.y * count)
-  end
-
-  private
-
-  def l(o)
-    Direction.new(-1 * o.y,  o.x)
-  end
-
-  def r(o)
-    Direction.new(o.y, -1 * o.x)
-  end
 end
 
 class Problem01 < ProblemBase
 
   def initialize
     @problem = '01'
-    @o = Direction.new(0, 1)
-    @locations = [] << Location.new(0, 0)
+    @person = Person.new
   end
 
   def run
     data.split(",").map { |step| Instruction.new(step) }.each do |instruction|
-      @o = instruction.turn(@o)
-      @locations <<  instruction.move(@o, @locations.last)
+      @person.follow_instruction(instruction)
     end
-    puts  "#{@locations.last.away} blocks away at #{@locations.last.to_s}"
+    puts  "#{@person.position.away} blocks away at #{@person.position.to_s}"
   end
 end
 
